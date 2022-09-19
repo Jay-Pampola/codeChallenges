1fd743d5d6af8f8aa0f64f2da179703cf932db07
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-09-20 00:21:12 +0800

    coding exercise

---
+++ codingExercise56.js
@@ -0,0 +1,11 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+const rainbow = document.querySelectorAll('h1 span');
+rainbow[0].style.color = colors[0];
+rainbow[1].style.color = colors[1];
+rainbow[2].style.color = colors[2];
+rainbow[3].style.color = colors[3];
+rainbow[4].style.color = colors[4];
+rainbow[5].style.color = colors[5];
+rainbow[6].style.color = colors[6];
