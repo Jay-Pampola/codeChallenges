c0ec5e3e907189e6d0a70978930f265dc6d79b64
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-09-19 21:49:26 +0800

    coding exercise

--- codingExercise.js
+++ codingExercise.js
@@ -679,5 +679,5 @@ const allEvens = nums => {
     return nums.every((numbers) => (numbers % 2 === 0));
 };
 console.log(allEvens([2, 4, 6, 8]));
-
-//codingExercise 51
+console.log(allEvens([1, 4, 6, 8]));
+console.log(allEvens([1, 2, 3]));
