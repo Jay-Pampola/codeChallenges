97957f4dfe2dbc42527ae28a77d202938bd61b2a
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-09-20 13:51:54 +0800

    codingExercise

---
+++ codingExercise57.js
@@ -0,0 +1,8 @@
+// WRITE YOUR CODE IN HERE:
+const hiLites = document.querySelectorAll('li');
+hiLites[0].setAttribute('class', 'highlight');
+hiLites[1].classList.remove('highlight');
+hiLites[2].setAttribute('class', 'highlight');
+hiLites[3].setAttribute('class', 'highlight');
+hiLites[4].classList.remove('highlight');
+hiLites[5].setAttribute('class', 'highlight');
