86fc4e27f66363fbbba664fd7389c386444d28d3
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-09-27 12:32:51 +0800

    coding exercise

--- codeChallenge-try.js
+++ codeChallenge-try.js
@@ -19,6 +19,7 @@ async function getBeans() {
 getBeans();
 console.log("Describe what happens with this `console.log()` statement as well."); */
 
+/*
 function buildMassiveString() {
     massiveString = Array(1000).fill("STRING").join();
 };
@@ -55,3 +56,73 @@ v.bestie = jimin;
 const bts = [jin, suga, jHope, rm, jimin, v, jungkook];
 
 console.log(bts);
+
+
+// Setup
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    records[id][prop] = value;
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+updateRecords(recordCollection, 2548, "artist", "");
+updateRecords(recordCollection, 1245, "tracks", "Addicted to Love");
+updateRecords(recordCollection, 2468, "tracks", "Free");
+updateRecords(recordCollection, 2548, "tracks", "");
+updateRecords(recordCollection, 1245, "albumTitle", "Riptide");
+
+
+console.log(recordCollection[2468].artist);
+console.log(recordCollection);
+*/
+
+// Only change code below this line
+// for loop
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        let arr = [];
+        for (let i = 1; i < (n + 1); i++) {
+            //console.log([i]);
+            arr.push(i);
+        }
+        return arr.reverse();
+    }
+}
+// recursion
+// Only change code below this line
+function countdownRecusrive(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArr = countdown(n - 1);
+        countArr.unshift(n)
+        return countArr;
+    }
+}
+// Only change code above this line
+
+console.log(countdownRecursive(10))
