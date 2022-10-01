0706c2a089a946c359fbc234ca8f48ca7e7b094d
Mielbert Jay Pampola <mil.pampola112311@gmail.com>
2022-10-01 20:48:24 +0800

    coding exercise

--- codingExercise51.js
+++ codingExercise51.js
@@ -1,3 +1,5 @@
+// Coding Exercise 51
+
 // Write your code in here:
 const image = document.getElementById('unicorn');
 const heading = document.getElementById('mainheading');
--- codingExercise56.js
+++ codingExercise56.js
@@ -1,3 +1,5 @@
+// Coding Exercise 56
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
 
 //YOU CODE GOES HERE:
